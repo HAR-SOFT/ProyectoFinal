388c31bdf7474ce83cd2ab83fb909cd85cb53aa7
Rodrigo <Rodrigo@Rodrigo>
2017-07-31 19:20:20 -0300

    Con estilos

---
+++ ProyectoFinal/vistas/js/app.js
@@ -0,0 +1,6 @@
+$('.dropdown-toggle').dropdown()
+// comment this
+$('[data-toggle="popover"]').popover({container: "body"});
+// uncomment this
+//$('[data-toggle="popover"]').popover({container: "#fixed-div"});
+$("popover-content").css("overflow-y", "auto");
